40700d9ef588e920ecd0498cad683602b1aedabe
u002092 <kartashevss01@yandexlyceum.ru>
2025-04-05 10:24:27 +0300

    init commit

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
-import os
-
-from flask import Flask
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
+from data.news import News
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-def index():
-    return "Привет от Дианочки-работяги"
+def main():
+    db_session.global_init("db/blogs.db")
+
+
+    @app.route("/")
+    def index():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.is_private != True)
+        return render_template("index.html", news=news)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    main()
+    app.run(port=8080, host='127.0.0.1')
